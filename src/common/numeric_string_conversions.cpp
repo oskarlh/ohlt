96031c0e3ab7cc489de88f79e64cf67273514b82
oskarlh <oskar@oskar.pm>
2025-07-28 03:46:04 +0200

    Fix GCC compile error

--- src/common/numeric_string_conversions.cpp
+++ src/common/numeric_string_conversions.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <charconv>
+#include <limits>
 
 std::optional<std::int64_t> clamp_signed_integer_from_string(
 	std::u8string_view valueString, std::int64_t min, std::int64_t max
